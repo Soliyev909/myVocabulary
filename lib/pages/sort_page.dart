2f46353380abe9aee67a8ba37cb2fa0c7b2497f8
NiMeItachi <nursultan.suleymanov.2005@gamil.com>
2023-03-01 08:35:50 +0500

    l

--- lib/pages/sort_page.dart
+++ lib/pages/sort_page.dart
@@ -1,4 +1,5 @@
 import 'package:my_vocabulary/model/vocabulary.dart';
+import 'package:my_vocabulary/model/word_model.dart';
 import 'package:my_vocabulary/pages/intro_page.dart';
 import 'package:my_vocabulary/servises/io_servises.dart';
 
@@ -8,7 +9,8 @@ class SortPage{
   void sortPage(){
     print('1. Sort by Date');
     print('2. Sort randomize');
-    print('3. Back');
+    print('3. Sort by Memorized');
+    print('4. Back');
 
     int command = io.number;
 
@@ -17,6 +19,18 @@ class SortPage{
       IntroPage();
     }else if(command == 2){
       Vocabulary.words.shuffle();
+      IntroPage();
+    }else if(command == 3){
+      List<Word> cash = Vocabulary.words.where((word) => word.memorized).toList();
+      int count = 1;
+      for(final item in cash){
+        item.word.forEach((key,value) {
+          print('$count. $key : $value');
+        });
+        count++;
+      }
+      print('1. Back');
+      int command = io.number;
       IntroPage();
     }else{
       IntroPage();
